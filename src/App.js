b3c73e6b00cff643a9dde9f088d509243c678b0b
gag <256sha9gag@gmail.com>
2023-09-12 13:17:31 +0300

    fix: use initDataUnsafe

--- src/App.js
+++ src/App.js
@@ -5,12 +5,11 @@ function App() {
   const { t } = useTranslation();
   const tg = window.Telegram.WebApp;
 
-  console.log(tg?.initData);
-
   return (
     <div className="d-flex flex-column p-2 justify-content-center container-sm">
       <h1 className="fs-5 text-center mb-2">{t('greeting')}</h1>
       <Buttons />
+      <div>{tg?.initDataUnsafe}</div>
     </div>
   );
 }
